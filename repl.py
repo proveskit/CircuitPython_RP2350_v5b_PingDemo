f2ee9e550bb20f9abf7db9ea9c0417715386af39
Michael Pham <61564344+Mikefly123@users.noreply.github.com>
2025-05-19 11:12:37 -0700

    Temp all_faces Fix
    This is a temporary reimplementation of all_faces in the `repl.py` to allow people to continue testing while we refactor these in PySquared

--- repl.py
+++ repl.py
@@ -18,15 +18,22 @@ try:
 except ImportError:
     import board
 
+try:
+    from typing import Union
+except Exception:
+    pass
+
 import os
 
 import lib.pysquared.functions as functions
 import lib.pysquared.nvm.register as register
+from lib.adafruit_drv2605 import DRV2605  ### This is Hacky V5a Devel Stuff###
 from lib.adafruit_mcp230xx.mcp23017 import (
     MCP23017,  ### This is Hacky V5a Devel Stuff###
 )
 from lib.adafruit_mcp9808 import MCP9808  ### This is Hacky V5a Devel Stuff###
 from lib.adafruit_tca9548a import TCA9548A  ### This is Hacky V5a Devel Stuff###
+from lib.adafruit_veml7700 import VEML7700  ### This is Hacky V5a Devel Stuff###
 
 # from lib.pysquared.Big_Data import AllFaces  ### This is Hacky V5a Devel Stuff###
 from lib.pysquared.cdh import CommandDataHandler
@@ -258,9 +265,95 @@ all_faces_on()
 
 tca = TCA9548A(i2c1, address=int(0x77))
 
-# all_faces = AllFaces(tca, logger)
+
+### This is Hacky V5a Devel Stuff###
+class Face:
+    def __init__(self, add: int, pos: str, tca: TCA9548A, logger: Logger) -> None:
+        self.tca: TCA9548A = tca
+        self.address: int = add
+        self.position: str = pos
+        self.logger: Logger = logger
+
+        # Use tuple instead of list for immutable data
+        self.senlist: tuple = ()
+        # Define sensors based on position using a dictionary lookup instead of if-elif chain
+        sensor_map: dict[str, tuple[str, ...]] = {
+            "x+": ("MCP", "VEML", "DRV"),
+            "x-": ("MCP", "VEML"),
+            "y+": ("MCP", "VEML", "DRV"),
+            "y-": ("MCP", "VEML"),
+            "z-": ("MCP", "VEML", "DRV"),
+        }
+        self.senlist: tuple[str, ...] = sensor_map.get(pos, ())
+
+        # Initialize sensor states dict only with needed sensors
+        self.sensors: dict[str, bool] = {sensor: False for sensor in self.senlist}
+
+        # Initialize sensor objects as None
+        self.mcp = None
+        self.veml = None
+        self.drv = None
+
+    def sensor_init(self, senlist, address) -> None:
+        if "MCP" in senlist:
+            try:
+                self.mcp: MCP9808 = MCP9808(self.tca[address], address=27)
+                self.sensors["MCP"] = True
+            except Exception as e:
+                self.logger.error("Error Initializing Temperature Sensor", e)
+
+        if "VEML" in senlist:
+            try:
+                self.veml: VEML7700 = VEML7700(self.tca[address])
+                self.sensors["VEML"] = True
+            except Exception as e:
+                self.logger.error("Error Initializing Light Sensor", e)
+
+        if "DRV" in senlist:
+            try:
+                self.drv: DRV2605 = DRV2605(self.tca[address])
+                self.sensors["DRV"] = True
+            except Exception as e:
+                self.logger.error("Error Initializing Motor Driver", e)
+
+
+class AllFaces:
+    def __init__(self, tca: TCA9548A, logger: Logger) -> None:
+        self.tca: TCA9548A = tca
+        self.faces: list[Face] = []
+        self.logger: Logger = logger
+
+        # Create faces using a loop instead of individual variables
+        positions: list[tuple[str, int]] = [
+            ("y+", 0),
+            ("y-", 1),
+            ("x+", 2),
+            ("x-", 3),
+            ("z-", 4),
+        ]
+        for pos, addr in positions:
+            face: Face = Face(addr, pos, tca, self.logger)
+            face.sensor_init(face.senlist, face.address)
+            self.faces.append(face)
+
+    def face_test_all(self) -> list[list[float]]:
+        results: list[list[float]] = []
+        for face in self.faces:
+            if face:
+                try:
+                    temp: Union[float, None] = (
+                        face.mcp.temperature if face.sensors.get("MCP") else None
+                    )
+                    light: Union[float, None] = (
+                        face.veml.lux if face.sensors.get("VEML") else None
+                    )
+                    results.append([temp, light])
+                except Exception:
+                    results.append([None, None])
+        return results
+
+
+all_faces = AllFaces(tca, logger)
 
 ## Onboard Temp Sensor ##
 mcp = MCP9808(i2c1, address=30)  # Not working for some reason
-
-### This is Hacky V5a Devel Stuff###
